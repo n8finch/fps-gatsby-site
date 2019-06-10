82e186609b83df911a71bb63ebf4ede7e6385602
Nate Finch <finchproservices@gmail.com>
2019-06-09 21:53:44 -0500

    initial commit

---
+++ data/siteConfig.js
@@ -0,0 +1,112 @@
+module.exports = {
+  siteTitle: 'Nate Finch',
+  siteDescription: `A little about Nate Finch and Finch Professional Services...`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Nate Finch',
+  twitterUsername: 'n8finch',
+  githubUsername: 'n8finch',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `"<em>I'd be a professional student if I could</em>..." <br /><br />
+  "<em>I'm a lot more function over form... I couldn't design my way out of a paper bag...</em>..." <br /><br />
+  I have been building websites since 2011. I love web development because it is the perfect mix of problem solving, building, and lifelong learning.<br/><br />
+  Currently VP of Development at The Digital Ring, I split my time between developing websites and developing developers. Prior to joining TDR, I freelanced as a sole proprietor (Finch Professional Services), worked with dozens of clients through platforms like Toptal and Codeable, and completed a brief stint as a web engineer with 10up.<br/><br />
+  I collect masters degrees (three so far), have traveled to six continents, lived on four, speak Spanish, and outside of work, family, friends, food, coffee, and cooking are my favorite activities. I live in Wisconsin with my wonderful wife and two daughters.<br /><br />
+  If you want to know more, I write at <strong><a href="https://n8finch.com" target="_blank">n8finch.com</a></strong>`,
+  skills: [
+    {
+      name: 'Working with Clients',
+      level: 90
+    },
+    {
+      name: 'Design Skills (yeah, not a designer)',
+      level: 10
+    },
+    {
+      name: 'JavaScript and PHP',
+      level: 80
+    },
+    {
+      name: 'Smoked Meats (especially Brisket)',
+      level: 90
+    },
+    {
+      name: 'Learning New Things',
+      level: 75
+    },
+    {
+      name: 'Forward Thinking, Keeping Up',
+      level: 70
+    },
+    {
+      name: 'Loves being a Dad',
+      level: 100
+    },
+    {
+      name: 'Enjoyment of coffee, barrel-aged beer, and scotch.',
+      level: 100
+    },
+  ],
+  jobs: [
+    {
+      company: "The Digital Ring",
+      begin: {
+        month: 'May',
+        year: '2018'
+      },
+      duration: null,
+      occupation: "VP of Development",
+      description: "Building custom websites and applications for clients, leading our development team, serving as best I can."
+  
+    }, {
+      company: "10up",
+      begin: {
+        month: 'June',
+        year: '2018'
+      },
+      duration: '2 mos',
+      occupation: "Web Engineer",
+      description: "Work with a world-class team of engineers and designers to create scalable, enterprise grade websites on WordPress."
+  
+    }, {
+      company: "toptal & Codeable",
+      begin: {
+        month: 'Oct',
+        year: '2016'
+      },
+      duration: 'no longer taking on clients',
+      occupation: "Fullstack WordPress Developer",
+      description: "Got accepted to these \"freelance guilds\", as I call them, working with clients from all over the world, all sorts of tasks and customizations."
+    },{
+      company: "FPS",
+      begin: {
+        month: 'Dec',
+        year: '2012'
+      },
+      duration: 'no longer taking on clients',
+      occupation: "Full-Stack Developer",
+      description: "Working with clients and agencies to build great websites and custom features. Mostly with WordPress."
+  
+    }
+    /* ... */
+  ],
+  social: {
+    twitter: "https://twitter.com/n8finch",
+    linkedin: "https://www.linkedin.com/in/natefinch",
+    github: "https://github.com/n8finch",
+    email: null
+  },
+  siteUrl: 'https://n8finch.com',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: null,
+  background_color: '#efefef',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Finch Professional Services',
+      url: '/',
+    }
+  ]
+}
